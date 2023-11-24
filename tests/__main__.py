c69645be19e63df6d9448037e69fcf75bba744fd
feluxe <felix@meyerwolters.de>
2023-11-24 03:45:09 +0100

    Run formatter

--- tests/__main__.py
+++ tests/__main__.py
@@ -8,16 +8,15 @@ import itertools
 # TODO: This should get some improvements. ;)
 
 
-async def produce_numbers(x, name='a'):
+async def produce_numbers(x, name="a"):
     await aio.sleep(random.uniform(0, 1))
     return [str(x) + name]
 
 
 async def main():
-
     # Test async chain
-    coros1 = (produce_numbers(i, 'a') for i in range(0, 3))
-    coros2 = (produce_numbers(i, 'b') for i in range(0, 3))
+    coros1 = (produce_numbers(i, "a") for i in range(0, 3))
+    coros2 = (produce_numbers(i, "b") for i in range(0, 3))
     futs1 = aio.as_completed([f for f in coros1])
     futs2 = aio.as_completed([f for f in coros2])
 
@@ -31,6 +30,6 @@ async def main():
         print(i)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     loop = aio.get_event_loop()
     loop.run_until_complete(main())
